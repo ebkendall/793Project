19d7fb7b3547cac57397a36a0bf9afd0d0779760
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2021-11-28 19:14:28 -0500

    still fixing things

--- roughDraftCode.R
+++ roughDraftCode.R
@@ -10,13 +10,6 @@ for(i in 0:12) {
 }
 m_3_6 = round(m_3_6, 4)
 
-# Probability Mass 3.10 (m_3_10)
-m_3_10 = rep(1,26)
-for(i in 0:25) {
-  m_3_10[i+1] = pbinom(6, 13, (i)/25) - pbinom(6,13, (i+1)/25)
-}
-m_3_10[26] = 0
-m_3_10 = round(m_3_10, 4)
 
 
 for (j in 1:10) {
@@ -28,35 +21,19 @@ for (j in 1:10) {
   temp = rep(0,50)
   for(k in 1:50) {
     ind = sort(sample(1:13, replace = T, prob = rep(1/13,13)))
-    x = X[j,ind]
+    x = sort(X[j,ind])
     x_m = median(x) 
     for(l in 1:13) {
-      temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_6[ind[l]]
+      temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_6[l]
     }
   }
   est_exp[j,1] = mean(temp)
   
-  # Method 3.10
-  temp = rep(0,50)
-  # making the symmetric distribution about the median
-  d_x = sort(X[j,])
-  d_x_med = d_x[7]
-  d_x = d_x[-7]
-  d_x = sort(c(d_x, 2*d_x_med - d_x, d_x_med))
-  
-  for(k in 1:50) {
-    ind = sort(sample(1:25, replace = T, prob = rep(1/25,25)))
-    #x = sort(sample(d_x, replace = T, prob = rep(1/25,25)))
-    x = d_x[ind]
-    x_m = median(x)
-    for(l in 1:25) {
-      temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_10[ind[l]]
-    }
-  }
-  est_exp[j,2] = mean(temp)
-
 }
 
+est_exp[,1]
+mean(est_exp[,1])
+sd(est_exp[,1])
 # 
 # temp = rep(0,50)
 # for(k in 1:50) {
@@ -68,6 +45,30 @@ for (j in 1:10) {
 # }
 # est_exp[j,1] = mean(temp)
 
+# Probability Mass 3.10 (m_3_10)
+m_3_10 = rep(1,26)
+for(i in 0:25) {
+  m_3_10[i+1] = pbinom(6, 13, (i)/25) - pbinom(6,13, (i+1)/25)
+}
+m_3_10[26] = 0
+m_3_10 = round(m_3_10, 4)
 
+# Method 3.10
+temp = rep(0,50)
+# making the symmetric distribution about the median
+d_x = sort(X[j,])
+d_x_med = d_x[7]
+d_x = d_x[-7]
+d_x = sort(c(d_x, 2*d_x_med - d_x, d_x_med))
 
+for(k in 1:50) {
+  ind = sort(sample(1:25, replace = T, prob = rep(1/25,25)))
+  #x = sort(sample(d_x, replace = T, prob = rep(1/25,25)))
+  x = d_x[ind]
+  x_m = median(x)
+  for(l in 1:25) {
+    temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_10[ind[l]]
+  }
+}
+est_exp[j,2] = mean(temp)
 
