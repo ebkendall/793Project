53fc6332b822ed3db734934bafec8adc807b38ac
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2021-12-02 09:40:34 -0500

    finished the simulation

--- roughDraftCode.R
+++ roughDraftCode.R
@@ -1,7 +1,10 @@
 set.seed(100)
 n = 13
 
-est_exp = matrix(nrow=10,ncol=2)
+# 10 Trials of sample size 13 (each row a trial)
+X_orig = matrix(rnorm(n*10), nrow = 10)
+
+# -----------------------------------------------------------------
 
 # Probability Mass 3.6 (m_3_6)
 m_3_6 = rep(-1,13)
@@ -10,65 +13,79 @@ for(i in 0:12) {
 }
 m_3_6 = round(m_3_6, 4)
 
+# Method 3.6
+X = t(apply(X_orig, 1, sort))
+med_X = X[,7] # the column of medians
+X = X - med_X 
+exp_val = X %*% as.matrix(m_3_6); print(round(exp_val, digits = 3))
+print(paste0("Average: ", round(mean(exp_val), digits = 3)))
+print(paste0("SD: ", round(sd(exp_val), digits = 3)))
 
-
-for (j in 1:10) {
-  
-  X = matrix(rnorm(n*10), nrow = 10)
-  
-  # Method 3.6
-  # Fix the Trial j sample and then bootstrap sample 50 times
-  temp = rep(0,50)
-  for(k in 1:50) {
-    ind = sort(sample(1:13, replace = T, prob = rep(1/13,13)))
-    x = sort(X[j,ind])
-    x_m = median(x) 
-    for(l in 1:13) {
-      temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_6[l]
-    }
-  }
-  est_exp[j,1] = mean(temp)
-  
-}
-
-est_exp[,1]
-mean(est_exp[,1])
-sd(est_exp[,1])
-# 
-# temp = rep(0,50)
-# for(k in 1:50) {
-#   x = sort(sample(X[j,], replace = T, prob = rep(1/13,13)))
-#   x_m = median(x) 
-#   for(l in 1:13) {
-#     temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_6[l]
-#   }
-# }
-# est_exp[j,1] = mean(temp)
+# -----------------------------------------------------------------
 
 # Probability Mass 3.10 (m_3_10)
-m_3_10 = rep(1,26)
-for(i in 0:25) {
+m_3_10 = rep(1,25)
+for(i in 0:24) {
   m_3_10[i+1] = pbinom(6, 13, (i)/25) - pbinom(6,13, (i+1)/25)
 }
-m_3_10[26] = 0
+m_3_10[25] = 0
 m_3_10 = round(m_3_10, 4)
 
-# Method 3.10
-temp = rep(0,50)
-# making the symmetric distribution about the median
-d_x = sort(X[j,])
-d_x_med = d_x[7]
-d_x = d_x[-7]
-d_x = sort(c(d_x, 2*d_x_med - d_x, d_x_med))
+# Setting up the symmetric form
+X_3_10 = matrix(nrow = 10, ncol = 25)
+small_X = t(apply(X_orig, 1, sort))[,-7]
+X_3_10[,1:12] = small_X
+X_3_10[,13] = med_X
+X_3_10[,14:25] = 2*med_X - small_X
+X_3_10 = t(apply(X_3_10, 1, sort))
 
-for(k in 1:50) {
-  ind = sort(sample(1:25, replace = T, prob = rep(1/25,25)))
-  #x = sort(sample(d_x, replace = T, prob = rep(1/25,25)))
-  x = d_x[ind]
-  x_m = median(x)
-  for(l in 1:25) {
-    temp[k] = temp[k] + (x[l] - x_m)^2 * m_3_10[ind[l]]
+X_3_10 = X_3_10 - med_X
+exp_val = X_3_10 %*% as.matrix(m_3_10); print(exp_val)
+print(paste0("Average: ", round(mean(exp_val), digits = 3)))
+print(paste0("SD: ", round(sd(exp_val), digits = 3)))
+
+
+# -----------------------------------------------------------------
+set.seed(100)
+n = 13
+
+# 10 Trials of sample size 13 (each row a trial)
+X_orig = matrix(rnorm(n*10), nrow = 10)
+
+# Smoothed Bootstrap
+
+#One trial so far
+# I need 50 replicates of 13 data points
+bootSample <- function(x, d, s_z) {
+  x_star = rep(NA, length(x))
+  c = 1/sqrt(1 + s_z) # value of c suggested in paper
+  for(i in 1:length(x)) {
+    z_i = runif(1, -d/2, d/2)
+    x_star[i] = mean(x) + c * (sample(x,1) - mean(x) + sd(x) * z_i)
   }
+  return(x_star)
 }
-est_exp[j,2] = mean(temp)
 
+d = c(0,0.25,0.5,1) # values for d
+s_z = d^2 / 12 # variance of a Uniform(-d/2, d/2)
+
+mean_estimates = matrix(nrow = 10, ncol = 4)
+for (j in 1:10) { # number of trials
+  x = X_orig[j,]
+  med = median(x)
+  sd = sd(x)
+  for(k in 1:4) { # length of d 
+    r_vec = rep(1,50)
+    for (i in 1:50) { # N = 50 MC simulations
+      temp = bootSample(x, d[k], s_z[k])
+      r_vec[i] = abs(median(temp) - med) / sd
+    }
+    mean_estimates[j,k] = mean(r_vec)
+  }
+  
+}
+
+colnames(mean_estimates) = c("d = 0", "d = 0.25", "d = 0.5", "d = 1")
+print(mean_estimates)
+print("Average:"); print(colMeans(mean_estimates))
+print("Standard Deviation:"); print(apply(mean_estimates, 2, sd))
